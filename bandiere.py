4bc34ddb5b0266dcade62231fb5bf577764891b7
YungMadz <filipmadz18@gmail.com>
2023-05-03 08:42:58 +0200

    Implementazione delle caratteristiche simbolo(bool)e pattern

--- bandiere.py
+++ bandiere.py
@@ -1,25 +1,24 @@
 import csv
 
-def carica_dizio(file_path):
+def carica_dizio():
     bandiere = {}
-    with open(file_path, 'r') as file:
+    with open('Bandierette.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             paese = row[0]
-            colori = row[1:]
-            bandiere[paese] = colori
+            caratteristiche = row[1:]
+            bandiere[paese] = caratteristiche
     return bandiere
 
-def akinator(colori, bandiere):
+def akinator(caratteristiche_input, bandiere):
     for paese, colori_bandiera in bandiere.items():
-        if set(colori) == set(colori_bandiera):
+        if set(caratteristiche_input) == set(colori_bandiera):
             return paese
     return 'Paese non trovato'
 
-file_path = 'bandierette.csv'
-bandiere = carica_dizio(file_path)
+bandiere = carica_dizio()
 
-colori_input = input('Inserisci i colori della bandiera separati da uno spazio: ').split()
-paese_indovinata = akinator(colori_input, bandiere)
+caratteristiche_input = input('Inserisci le caratteristiche della bandiera separati da uno spazio: ').split()
 
+paese_indovinata = akinator(caratteristiche_input, bandiere)
 print('La bandiera appartiene a:', paese_indovinata)
