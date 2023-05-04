129482635a5382939002768353a7fc3794de9d0f
Davide Guerrazzi <g.davide2005@gmail.com>
2023-05-04 17:46:12 +0200

    Creazione del sito iniziale

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1dev",
     packages=['bandiere'],
     package_dir={'bandiere': 'src/bandiere'},
-    package_data={'bandiere': ['data/*.csv']},
+    package_data={'bandiere': ['data/*']},
     author="balordi LLC",
     license="MIT",
     install_requires=install_requires,
--- src/bandiere/views.py
+++ src/bandiere/views.py
@@ -1,38 +1,15 @@
 from bandiere import app
+from bandiere.gestione_bandiere import carica_dizio, akinator
 
-from flask import render_template
-
-# bandiere.py
-import csv
-import pkg_resources
-
-def carica_dizio():
-    bandiere = {}
-    
-    csv_file = pkg_resources.resource_filename('bandiere', 'data/Bandierette.csv')
-    
-    with open('Bandierette.csv', 'r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            paese = row[0]
-            caratteristiche = row[1:]
-            bandiere[paese] = caratteristiche
-    return bandiere
-
-def akinator(caratteristiche_input, bandiere):
-    for paese, colori_bandiera in bandiere.items():
-        if set(caratteristiche_input) == set(colori_bandiera):
-            return paese
-    return 'Paese non trovato'
+from flask import render_template, request
 
 bandiere = carica_dizio()
-
-caratteristiche_input = input('Inserisci le caratteristiche della bandiera separati da uno spazio: ').split()
-
-paese_indovinata = akinator(caratteristiche_input, bandiere)
-print('La bandiera appartiene a:', paese_indovinata)
-# /bandiere.py
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def index_post():
+    p = request.form['prompt']
+    return render_template("index.html", data=akinator(p, bandiere))
